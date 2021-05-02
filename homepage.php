3603c98c89cb93a6b972e4826992d0f21857f79d
sayu77 <62358822+sayu77@users.noreply.github.com>
2021-05-02 15:39:07 -0400

    Update homepage.php
    dummy homepage to actual homepage

--- homepage.php
+++ homepage.php
@@ -8,9 +8,28 @@ if (isset($_SESSION['ID']) && isset($_SESSION['Username'])) {
 <html>
 <head>
 	<title>HOME</title>
+	<link rel="stylesheet" href="project4.css" />
 </head>
 <body>
-     <h1>Welcome, <?php echo $_SESSION['Name']; ?></h1>
+    <h1>Welcome, <?php echo $_SESSION['Name']; ?></h1>
+    <div id=navigation>
+    	<div class=col>
+    		<a href="user.php"><img src="user.png"></a>
+    	</div>
+    	<div class=col>
+    		<a href="#cart.php"><img src="cart.jpg"></a>
+    	</div>
+	</div>
+   	<main>
+   		<aside id=left>
+    		<a href="PrePaidParking.php"><img src="parkingspace.jpg"></a>
+    		PrePaid Parking
+ 		</aside>
+    	<aside id=right>
+    	 	<a href="#car_rental.php"><img src="carrent.jpg"></a>
+    	 	Car Rental
+		</aside>
+	</main>
 </body>
 </html>
 
@@ -20,3 +39,4 @@ if (isset($_SESSION['ID']) && isset($_SESSION['Username'])) {
      exit();
 }
  ?>
+
