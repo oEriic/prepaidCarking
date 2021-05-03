8667369374b50e0e00898d335f2183bdf9cd5cc7
Eric Oh <77398304+oEriic@users.noreply.github.com>
2021-05-02 23:33:23 -0400

    added ex Date

--- creditCard.php
+++ creditCard.php
@@ -39,6 +39,29 @@
                 <label for="secCode">Security Code</label>
                 <input type="password" name="code" id="secCode" maxlength="4">
                 </br>
+                <label for="date">EX Date</label>
+                
+                <select tabindex="12" id="exMonth" style="margin-left: 10px;">
+                    <option>01-Jan</option>
+                    <option>02-Feb</option>
+                    <option>03-Mar</option>
+                    <option>04-Apr</option>
+                    <option>05-May</option>
+                    <option>06-Jun</option>
+                    <option>07-Jul</option>
+                    <option>08-Aug</option>
+                    <option>09-Sep</option>
+                    <option>10-Oct</option>
+                    <option>11-Nov</option>
+                    <option>12-Dec</option>
+                </select>
+                <select tabindex="4" id="exDate" style="margin-left: 10px;">
+                    <option>2021</option>
+                    <option>2022</option>
+                    <option>2023</option>
+                    <option>2024</option>
+                </select>
+
                 <select tabindex="11" id="CardType" style="margin-left: 10px;">
                     <option value="AmEx">American Express</option>
                     <option value="Discover">Discover</option>
