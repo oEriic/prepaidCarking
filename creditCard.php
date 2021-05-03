07a3aa72084a8bcb6d99233b19ec5f69e5169dae
Eric Oh <77398304+oEriic@users.noreply.github.com>
2021-05-03 00:28:13 -0400

    final update for creditcard

--- creditCard.php
+++ creditCard.php
@@ -7,25 +7,7 @@
 </head>
 
 <body>
-    <table>
-        <tr>
-            <td style="padding-right: 30px;">American Express</td>
-            <td>3400 0000 0000 009</td>
-        </tr>
-        <tr>
-            <td>Discover</td>
-            <td>6011 0000 0000 0004</td>
-        </tr>
-        <tr>
-            <td>MasterCard</td>
-            <td>5500 0000 0000 0004</td>
-        </tr>
-        <tr>
-            <td>Visa</td>
-            <td>4111 1111 1111 1111</td>
-        </tr>
-    </table>
-    <hr />
+    
     <div class="cardInfo">
         <form action="index.html" method="POST" id="creditCard">
             <fieldset>
