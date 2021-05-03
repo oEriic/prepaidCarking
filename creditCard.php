77afc8f11567d5272e4b419f64d27c2279a37813
Eric Oh <77398304+oEriic@users.noreply.github.com>
2021-05-02 23:27:19 -0400

    added Pin section

--- creditCard.php
+++ creditCard.php
@@ -35,69 +35,9 @@
                 <label for="name">Name: </label>
                 <input type="text" name="userName" id="name" maxlength="16" />
                 </br>
-                <label for="stAddress">Street Address: </label>
-                <input type="text" name="stAddress" id="stAddress" maxlength="50" />
-                </br>
-                <label for="city">City: </label>
-                <input type="text" name="city" id="stAddress" maxlength="20" />
-                <label for="state">State: </label>
-                <select id="state" style="margin-left: 10px;">
-                    <option>AL</option>
-                    <option>AK</option>
-                    <option>AZ</option>
-                    <option>AR</option>
-                    <option>CA</option>
-                    <option>CO</option>
-                    <option>CT</option>
-                    <option>DE</option>
-                    <option>FL</option>
-                    <option>GA</option>
-                    <option>HI</option>
-                    <option>ID</option>
-                    <option>IL</option>
-                    <option>IN</option>
-                    <option>IA</option>
-                    <option>KS</option>
-                    <option>KY</option>
-                    <option>LA</option>
-                    <option>ME</option>
-                    <option>MD</option>
-                    <option>MA</option>
-                    <option>MI</option>
-                    <option>MN</option>
-                    <option>MS</option>
-                    <option>MO</option>
-                    <option>MT</option>
-                    <option>NE</option>
-                    <option>NV</option>
-                    <option>NH</option>
-                    <option>NJ</option>
-                    <option>NM</option>
-                    <option>NY</option>
-                    <option>NC</option>
-                    <option>ND</option>
-                    <option>OH</option>
-                    <option>OK</option>
-                    <option>OR</option>
-                    <option>PA</option>
-                    <option>RI</option>
-                    <option>SC</option>
-                    <option>SD</option>
-                    <option>TN</option>
-                    <option>TX</option>
-                    <option>UT</option>
-                    <option>VT</option>
-                    <option>VA</option>
-                    <option>WA</option>
-                    <option>WV</option>
-                    <option>WI</option>
-                    <option>WY</option>
-                </select>
-                <label for="zip">ZIP Code: </label>
-                <input type="text" name="ZIP" id="zipCode" maxlength="5" />
-                </br>
-                <label for="phone">Phone: </label>
-                <input type="text" name="phone" id="phone" maxlength="10" />
+            
+                <label for="secCode">Security Code</label>
+                <input type="password" name="code" id="secCode" maxlength="4">
                 </br>
                 <select tabindex="11" id="CardType" style="margin-left: 10px;">
                     <option value="AmEx">American Express</option>
